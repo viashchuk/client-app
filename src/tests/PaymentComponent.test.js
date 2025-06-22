3e2840f7a58c277be181aed19533dca3298f45f0
Viki <victoriya.dev@icloud.com>
2025-06-22 22:09:08 +0200

    Update PaymentComponent.test.js

--- src/tests/PaymentComponent.test.js
+++ src/tests/PaymentComponent.test.js
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import Payment from '../components/Payment'
 import { useCart } from '../hooks/useCart'
 import axios from 'axios'
